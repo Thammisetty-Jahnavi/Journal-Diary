bb87d4fce430bb9951c981e2f16bf572dd1fcdc7
jahnavi <thammisettyjahnavi36@gmail.com>
2025-08-06 14:09:43 +0530

    journal diary

--- diary_project/env/bin/activate.fish
+++ diary_project/env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV '/mnt/c/Users/SRIT CSE/Desktop/Journal/JournalProject/diary_project/env'
+set -gx VIRTUAL_ENV /mnt/c/Users/dell/Desktop/Journal/JournalProject/diary_project/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
